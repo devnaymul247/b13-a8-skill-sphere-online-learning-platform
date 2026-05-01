bd1dd377218c18cf06ff4228a96f08920c83bc55
Namul <namul@Namuls-MacBook-Air.local>
2026-05-02 02:36:55 +0600

    deployed in vercel with json data

---
+++ src/components/PopularCourses.jsx
@@ -0,0 +1,14 @@
+
+
+const PopularCourses = async () => {
+    const res = await fetch('https://b13-a8-skill-sphere-online-learning.vercel.app/data.json');
+    const courses = await res.json();
+    const topCourses = courses.slice(0, 6); // Get the top 6 courses
+    return (
+        <div>
+            <h1 className='text-3xl font-bold text-center my-6'> Popular Courses </h1>
+        </div>
+    );
+};
+
+export default PopularCourses;
